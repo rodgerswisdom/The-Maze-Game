23f70ea8c4206d68c168b51be3c59b14c9e66449
rodgerswisdom <trulyhawona@gmail.com>
2024-07-08 11:12:17 +0300

    feat: Create a window

--- main.c
+++ main.c
@@ -21,11 +21,25 @@ int main( int argc, char **argv)
     SDL_Window *window = NULL;
     SDL_Surface *screenSurface = NULL;
 
+    /* Initialize SDL video subsystems
+     * Returns 0 on success or a negative number on Error
+    */
+
     if (SDL_Init( SDL_INIT_VIDEO) != 0))
     {
         fprintf( stderr, "SDL failed to initialize: %s\n", SDL_GetError() );
         return (1);
     }
+
+    /* Create Window */
+
+    window = SDL_CreateWindow("The Maze Game"), /* Name of the window*/
+                            SDL_WINDOWPOS_UNDEFINED, /* Position x of the window*/
+                            SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                            HEIGHT, /* Height of the window in pixels*/
+                            WIDTH,  /* Width of the window in pixels*/
+                            SDL_WINDOW_SHOWN); /* flag to make sure window is shown on creation*/
+
 
     return (0);
 }
