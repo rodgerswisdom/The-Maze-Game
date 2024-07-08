5c0c6a7b0722ead9c2dd9e6e6da6d79685c4bb4f
rodgerswisdom <trulyhawona@gmail.com>
2024-07-08 11:07:17 +0300

    feat: Initialize SDL video subsystems

--- main.c
+++ main.c
@@ -5,3 +5,27 @@
 #else
 #include <SDL2/SDL.h> /* MacOS and GNU/Linux Specific Library*/
 #endif // _WIN32
+
+/* https://rb.gy/oskvg9 */
+
+/* Set Constants*/
+
+#define HEIGHT 800
+#define WIDTH 600
+#define DELAY 5000
+
+int main( int argc, char **argv)
+{
+    /* Initialize data*/
+
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+
+    if (SDL_Init( SDL_INIT_VIDEO) != 0))
+    {
+        fprintf( stderr, "SDL failed to initialize: %s\n", SDL_GetError() );
+        return (1);
+    }
+
+    return (0);
+}
