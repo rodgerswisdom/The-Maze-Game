3947295b2f059f5848f23fd477809e3503a67b01
rodgerswisdom <trulyhawona@gmail.com>
2024-07-08 10:48:27 +0300

    feat: include libraries

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
+#include <stdio.h> /*prinf and fprintf*/
 
-#include <SDL2/SDL.h>
-
-
-int main(int argc, char *argv[])
-{
-
-	// returns zero on success else non-zero
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("error initializing SDL: %s\n", SDL_GetError());
-	}
-	SDL_Window* win = SDL_CreateWindow("GAME",
-									SDL_WINDOWPOS_CENTERED,
-									SDL_WINDOWPOS_CENTERED,
-									1000, 1000, 0);
-	while (1)
-		;
-
-	return 0;
-}
+#ifndef _WIN32
+#include <SDL/SDL.h> /*Windows Specific Library*/
+#else
+#include <SDL2/SDL.h> /* MacOS and GNU/Linux Specific Library*/
+#endif // _WIN32
